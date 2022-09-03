cfc35f8795313ee90e9b87e77b2d51d95e054f34
pranava <pranavagayathriaavuti@gmail.com>
2022-09-03 17:15:11 +0530

    initial commit

---
+++ DOCTOR/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+
+# Register your models here.
+from DOCTOR.models import *
+admin.site.register(Docter)
+admin.site.register(Prescription2)
+# Register your models here.
